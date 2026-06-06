52da126e69886ddb250d509b4541de5501d90ba3
mikizalesie <mikizalesie@o2.pl>
2026-06-07 00:42:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,19 @@
-const CACHE='ebike-manager-pro-v1';
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
+const CACHE_NAME = "ebike-pro-v14";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]))
+  );
 });
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
